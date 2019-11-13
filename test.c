abec05a9322ac2d4c0b5cae8870db16da9cc8d35
JakobFaarGreg <56345889+JakobFaarGreg@users.noreply.github.com>
2019-11-13 12:23:59 +0100

    Fikset Patricks mistake
    Patrick vidste ikke at return 0 skal stå sidst i main

--- test.c
+++ test.c
@@ -4,12 +4,13 @@ void Test(void);
 
 int main(void) {
     int jakobG, victor, kevinS, freddyH, christianH, marcoL, patrickO;
-    return 0;
+
     int i;
     /* Hej, Victor er træls */
     /* Hej, men farvel igen*/
     for(i = 0; i <=5; i++)
         printf("hej nummer %d",i);
+    return 0;
 }
 
 void Test(void) {
