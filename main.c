8a22ef71ae5a2e661d8b59f65c38a68a37b6aeaf
Pattrigue <Pattrigue@users.noreply.github.com>
2019-11-22 14:37:55 +0100

    Opdateret main.c

--- main.c
+++ main.c
@@ -2,47 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum unit { g, kg };
+#define MAX_DIRECTORY_LENGTH 80
+#define MAX_NAME_LENGTH 50
+#define MAX_INGREDIENTS_CHARS 1024
+#define MAX_PROCEDURE_CHARS 1024
+#define MAX_TAGS 10
+
+typedef enum { g, kg } unit;
 const char *files[] = { "test", "test2" };
 const char *dir = "./recipes/";
 
-typedef struct {
-    char *name;
-    int amount;
-    enum unit unit;
-} Ingredient;
-
+/* Recipe struct which contains all the info */
 typedef struct {
     char *name;
     char *procedure;
-    Ingredient *ingredients;
-} Recipe;
+    char *ingredients;
+    char *tags;
+    int time;
+} recipe;
 
-void read_database(void);
+recipe *get_database();
 char *get_file_directory(const char *fileName);
 
-int main(void) {
-    read_database();
-    return 0;
+int main(void) {    
+    /* Initialize the database */
+    recipe *recipe_database = get_database();
+
+    /* Example. Prints the name of the first recipe in the databaes. */
+    printf("Name of the first recipe in the database is: %s.\n", recipe_database[0].name);
+
+    /* When we're done with the database, we need to free the memory */
+    free(recipe_database);
+    return EXIT_SUCCESS;
 }
 
-void get_recipe_data(FILE *fp) {
+recipe get_recipe_data(FILE *fp) {
+    recipe recipe;
+    /*printf("------------------------------\n"); */
+
     /* Temporary static array declarations */
-    char title[1024];
-    char ingredients[1024];
-    char procedure[1024];
+    char name_buffer[MAX_NAME_LENGTH];
+    char ingredients_buffer[MAX_INGREDIENTS_CHARS];
+    char procedure_buffer[MAX_PROCEDURE_CHARS];
+    char tags_buffer[MAX_TAGS];
 
-    /* Scan through the file until the symbol ; is encountered for each of the three strings */
-    fscanf(fp, "%[^;]; %[^;]; %[^;];", title, ingredients, procedure);
+    /* Scan through the file until the symbol ; is encountered for each of the four strings */ 
+    /* Also note how recipe.time is written to directly, because it is easy to assign to */
+    fscanf(fp, "%[^;]; %[^;]; %[^;]; %[^;]; %d", name_buffer, ingredients_buffer, procedure_buffer, tags_buffer, &recipe.time);
 
-    /* Print the result */
-    printf("Title:\n- %s\n\nIngredients:\n%s\n\nProcedure:\n%s", title, ingredients, procedure);
+    /* Allocate memory for the recipe struct */
+    recipe.name        = calloc(strlen(name_buffer),        sizeof(char));
+    recipe.ingredients = calloc(strlen(ingredients_buffer), sizeof(char));
+    recipe.procedure   = calloc(strlen(procedure_buffer),   sizeof(char));
+    recipe.tags        = calloc(strlen(tags_buffer),        sizeof(char));
+
+    /* Copy the items into the struct */
+    strcpy(recipe.name,        name_buffer);
+    strcpy(recipe.ingredients, ingredients_buffer);
+    strcpy(recipe.procedure,   procedure_buffer);
+    strcpy(recipe.tags,        tags_buffer);
+
+    /* Print the result
+    printf("Title:\n- %s\n\nIngredients:\n%s\n\nProcedure:\n%s\n\nTags: %s\n\nTime: %d minutes.\n", 
+    recipe.name, recipe.ingredients, recipe.procedure, recipe.tags, recipe.time); */
+    return recipe;
 }
 
 char *get_file_directory(const char *fileName) {
+    /* Initialize directory name (string) */
+    char *directory_name = malloc(sizeof(char) * MAX_DIRECTORY_LENGTH);
+    
     /* Concatenate the recipe directory with the name of the file and the .txt extension */ 
     /* Format: ./recipes/fileName.txt where the dot represents the current directory */
-    char *directory_name = malloc(sizeof(char) * 80);
     strcpy(directory_name, dir);
     strcat(directory_name, fileName);
     strcat(directory_name, ".txt");
@@ -50,10 +81,12 @@ char *get_file_directory(const char *fileName) {
     return directory_name;
 }
 
-void read_database(void) {
+recipe *get_database() {
     /* Get the amount of files to read based on the files array defined at the top */
-    int n = sizeof(files) / sizeof(files[0]);
-    int i;
+    int i, n = sizeof(files) / sizeof(files[0]);
+
+    /* Initialize the array of recipe structs (the database) */
+    recipe *recipe_database = malloc(sizeof(recipe) * n);
 
     for (i = 0; i < n; i++) {
         /* Initialize file variable and get the file directory as a string */
@@ -64,9 +97,10 @@ void read_database(void) {
         fp = fopen(fileName, "r");
 
         /* Print the data from the file */
-        get_recipe_data(fp);
+        recipe_database[i] = get_recipe_data(fp);
         
         /* Close the file stream after reading */
         fclose(fp);
     }
+    return recipe_database;
 }
