9afdb0c91642bd811ae7b2cfb70d97f1f34cd2bb
mkju19 <mkju19@student.aau.dk>
2019-11-25 15:06:48 +0100

    Update randomizer.c

--- randomizer.c
+++ randomizer.c
@@ -3,9 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_RECIPES 10
-#define MAX_TAGS 8
-
 struct recipe{
     char name[50];
     char procedure[50];
@@ -15,52 +12,53 @@ struct recipe{
 };
 typedef struct recipe recipe;
 
-void randomizer(recipe all_recipes[], int ugeplan[],char *user_tags);
+int *randomizer(recipe all_recipes[], char *user_tags, int number_of_recipes);
 recipe make_recipe(char *name, char *ingredients, char *procedure, char *tags, int time);
 int tags_match (char *user_tags, char *recipe_tags);
 int is_duplicate(int i, int k, int *ugeplan);
 
 int main(void){
-    int ugeplan[7]; 
-    int placeholder, i;
-
-    recipe all_recipes[MAX_RECIPES];
+    int *ugeplan; 
+    int recipe_number, i;
+    int number_of_recipes = 9;
+    char *user_tags = "!&";
+    recipe all_recipes[9];
 
     srand(time(NULL));
     
-    all_recipes[0] = make_recipe("kebabrulle", "lav den", "kebab", "&!", 20);
-    all_recipes[1] = make_recipe("kebabsovs", "just do it", "sovs", "€<", 21);
-    all_recipes[2] = make_recipe("kødsovs", "bland sovs og kød", "sovs", "<-!+", 15);
-    all_recipes[3] = make_recipe("ulle", "bland og bland", "vandmand", "!€<-+", 44);
-    all_recipes[4] = make_recipe("Boller", "lav boller", "", "!<", 20);
-    all_recipes[5] = make_recipe("Pizza", "lav den plz", "tomater", "-+!<", 20);
-    all_recipes[6] = make_recipe("bagekartofler", "smid det vaek", "kartoffel", "&!", 20);
+    all_recipes[0] = make_recipe("kebabrulle", "lav den", "kebab", "&!&", 20);
+    all_recipes[1] = make_recipe("kebabsovs", "just do it", "sovs", "€<&", 21);
+    all_recipes[2] = make_recipe("kødsovs", "bland sovs og kød", "sovs", "&<-!+", 15);
+    all_recipes[3] = make_recipe("ulle", "bland og bland", "vandmand", "!€<&-+", 44);
+    all_recipes[4] = make_recipe("Boller", "lav boller", "", "!<&", 20);
+    all_recipes[5] = make_recipe("Pizza", "lav den plz", "tomater", "&-+!<", 20);
+    all_recipes[6] = make_recipe("bagekartofler", "smid det vaek", "kartoffel", "!&", 20);
     all_recipes[7] = make_recipe("kebabbolle", "make it good", "kebarulleboi", "!*&", 10);
-    all_recipes[8] = make_recipe("vand", "drik", "kun vand", "!*<", 25);
+    all_recipes[8] = make_recipe("vand", "drik", "kun vand", "!*<&", 25);
     
-    printf("entered tags = !");
-    randomizer(all_recipes, ugeplan, "!");
+    printf("entered tags = %s\n", user_tags);
+    ugeplan = randomizer(all_recipes, user_tags, number_of_recipes);
 
     for (i = 0; i < 7; i++){
-        placeholder = ugeplan[i]; 
+        recipe_number = ugeplan[i]; 
         printf("name: %s\n"
                "Ingredienser: %s\n"
                "Opskrift: %s\n"
                "tags: %s\n"
-               "tid: %d\n\n", all_recipes[placeholder].name, all_recipes[placeholder].ingredients,
-                        all_recipes[placeholder].procedure,
-                        all_recipes[placeholder].tags, all_recipes[placeholder].time);
+               "tid: %d\n\n", all_recipes[recipe_number].name, all_recipes[recipe_number].ingredients,
+                              all_recipes[recipe_number].procedure, all_recipes[recipe_number].tags, 
+                              all_recipes[recipe_number].time);
     }   
     return 0;
 }
 
 
-void randomizer(recipe all_recipes[], int ugeplan[], char *user_tags){
+int *randomizer(recipe all_recipes[], char *user_tags, int number_of_recipes){
     int i = 0, k, n = 0;
-    
+    int *ugeplan = malloc(sizeof(int) * 7);
     
     for (i = 0 ; i < 7 ; i++){
-        n = ugeplan[i] = rand() % MAX_RECIPES;
+        n = ugeplan[i] = rand() % number_of_recipes;
         for (k = 0 ; k < 7 ; k++){
             if (is_duplicate(i, k, ugeplan) || !(tags_match(user_tags, all_recipes[n].tags))){
                 i--;
@@ -68,6 +66,7 @@ void randomizer(recipe all_recipes[], int ugeplan[], char *user_tags){
             }
         }
     }
+    return ugeplan;
 }
 
 int is_duplicate(int i, int k, int *ugeplan){
@@ -83,22 +82,15 @@ int tags_match (char *user_tags, char *recipe_tags){
     int i, n = 0;
     int tag_len = strlen(user_tags);
 
-    printf("last user tag: %c\n", user_tags[tag_len]);
-    printf("last recipe tag: %c\n", recipe_tags[strlen(recipe_tags)]);
-
     for(i = 0; i < strlen(recipe_tags); i++){
         if(strchr(user_tags, recipe_tags[i]) != NULL)
             n++;
     }
-    if (n == tag_len){
-        printf("------match found------\n");
+    if (n == tag_len)
         return 1;
-    }
-    else{
-        printf("------mismatch found------\n");
+    else
         return 0;
-    }
-   
+    
 }
 
 recipe make_recipe(char *name, char *procedure, char *ingredients, char *tag, int time){
